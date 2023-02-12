1eba2ff188836bf3b7f7280179ba51045de89fdf
thoithoiba <70210729+thoithoiba@users.noreply.github.com>
2023-02-12 17:05:09 +0530

    modified:   src/components/App.js     modified:   src/components/Heading.js 	modified:   src/components/InputQuery.js 	modified:   src/components/SubHeading.js 	modified:   src/components/SubmitButton.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import InputQuery from './InputQuery';
+import Heading from './Heading';
+import SubHeading from './SubHeading';
+import SubmitButton from './SubmitButton';
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <InputQuery />
+      <Heading />
+      <SubHeading />
+      <SubmitButton />
     </div>
   )
 }
